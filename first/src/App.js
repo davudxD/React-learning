f808b2297f8924cf39b460ea766d881bd2d0766d
davudxD <carovacdavud6@gmail.com>
2023-04-05 19:47:10 +0200

    .

--- first/src/App.js
+++ first/src/App.js
@@ -1,8 +1,7 @@
-import React from "react";
-import "./App.css";
-import ExpenseItem from "./components/ExpenseItem.js";
+import React from 'react'
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -25,31 +24,18 @@ function App() {
     },
   ];
 
+
+
+
+
   return (
-    <div className="App">
-      <h1>Hi</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
-}
+
+  }
+
 
 export default App;
