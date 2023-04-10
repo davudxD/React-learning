b17ade84da43a0ae0e0881a18537a6fbb599c39d
davudxD <carovacdavud6@gmail.com>
2023-04-10 23:07:30 +0200

    .

--- first/src/components/Expenses/ExpenseItem.js
+++ first/src/components/Expenses/ExpenseItem.js
@@ -22,7 +22,7 @@ console.log(title)
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
+      
     </Card>
   );
 }
