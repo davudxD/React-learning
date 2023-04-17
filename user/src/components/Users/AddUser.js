90acbbdfabb836622138d49c4e617d1c146af044
davudxD <carovacdavud6@gmail.com>
2023-04-17 23:58:07 +0200

    styles

--- user/src/components/Users/AddUser.js
+++ user/src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../UI/Card";
+import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
 
@@ -8,7 +9,7 @@ const addUserHandler = (event) =>{
 }
 
     return (
-      <Card>
+      <Card className={classes.input} >
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
           <input id="username" type="text" />
