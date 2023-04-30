6ab9699e9ca45d0b06fb96749219dbcbc85bc58e
davudxD <carovacdavud6@gmail.com>
2023-04-30 14:15:05 +0200

    useEffect clean up function

--- 01-starting-project/src/components/Login/Login.js
+++ 01-starting-project/src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(()=>{
+
+    const identifier = setTimeout(() => {    
+      console.log('hi')
       setFormIsValid(
       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    );}, 500)
+
+    return () => {
+      console.log("Clean Up");
+      clearTimeout(identifier);
+    }
+
   }, [enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
