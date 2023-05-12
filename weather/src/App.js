38d94f013555375656cd6cf76868c2bdd5cd2818
davudxD <carovacdavud6@gmail.com>
2023-05-12 17:44:54 +0200

    Celsius converter

--- weather/src/App.js
+++ weather/src/App.js
@@ -9,32 +9,33 @@ const App = () => {
 
   const getWeather = (event) => {
     if (event.key === "Enter") {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`)
       .then(res => res.json())
       .then(data => {setWeatherData(data)
       console.log(data);});
     }
   }
 
+  const calculateCelsius = (fahrenheitTemp) => {
+    return (fahrenheitTemp - 32) / 1.8
+  };
 
   return (
     <div className='wrapper'>
       <input type="text"
        onChange={e => {setCity(e.target.value)
-      console.log(e.target.value)}}
+      }}
         placeholder='Enter your city'
          value={city}
          onKeyPress={getWeather} />
          
       
       {typeof weatherData.main === "undefined" ? (
-         
-            <p>Enter city</p>
-      
+            <p className='enter_city'>Enter city</p>
       ):(
      <div>
             <p>City: {weatherData.name}</p>
-            <p>Temperatura:{Math.round(weatherData.main.temp)}C</p>
+            <p>Temperatura: {Math.round(calculateCelsius(weatherData.main.temp))}C</p>
             <p>{weatherData.weather[0].main}</p>
      </div>
       )}
