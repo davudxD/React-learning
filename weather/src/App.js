bf257e2accaa9ac8216252396fdfde9323074cc4
davudxD <carovacdavud6@gmail.com>
2023-05-14 23:40:19 +0200

    Updates

--- weather/src/App.js
+++ weather/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   
   const [weatherData, setWeatherData] = useState([{}]);
   const [city, setCity] = useState("");
-  const [icon, setIcon] = useState("");
+  // const [icon, setIcon] = useState("");
 
   const getWeather = (event) => {
     if (event.key === "Enter") {
@@ -15,43 +15,45 @@ const App = () => {
         .then(data => {
           setWeatherData(data)
           console.log(data);
+          setCity("")
         });
     }
   }
 
 
+
   const handleInput = (e) =>{
-    setCity(e.target.value)
+    setCity(e.target.value);
   }
 
   const calculateCelsius = (fahrenheitTemp) => {
     return (fahrenheitTemp - 32) / 1.8
   };
 
-  const cloudy = () => {
-    if (weatherData.weather[0].main === "Clouds") {
-      setIcon("https://icons-for-free.com/iconfiles/png/512/cloudy+foggy+weather+icon-1320196574869514194.png")
-    } else {
-      setIcon("https://openweathermap.org/img/wn/03n.png")
-    }
-  }
+  // const cloudy = () => {
+  //   if (weatherData.weather[0].main === "Clouds") {
+  //     setIcon("https://icons-for-free.com/iconfiles/png/512/cloudy+foggy+weather+icon-1320196574869514194.png")
+  //   } else {
+  //     setIcon("https://openweathermap.org/img/wn/03n.png")
+  //   }
+  // }
 
   return (
     <div className='wrapper'>
       <input type="text"
         onChange={handleInput}
-        placeholder='Enter your city'
+        placeholder='Enter your city or country name'
         value={city}
         onKeyPress={getWeather} />
 
 
       {typeof weatherData.main === "undefined" ? (
-        <p className='enter_city'>Enter city or country</p>
+          <p className='enter_city'>Enter city or country name</p>
       ) : (
-        <div>
-          <p>City: {weatherData.name}</p>
+        <div className='results'>
+          <p>{weatherData.name}</p>
             <p>Temperatura: {Math.round(calculateCelsius(weatherData.main.temp))}°C</p>
-          <div><img src={icon} /></div>
+          {/* <div><img src={icon} /></div> */}
           <p>{weatherData.weather[0].main}</p>
         </div>
       )}
