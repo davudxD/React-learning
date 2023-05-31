cae2964a6d4c4d5a4b7f388cfa0228aa0efdcdc0
davudxD <carovacdavud6@gmail.com>
2023-05-31 11:33:34 +0200

    food order app - adding meal items

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import classes from './AvailableMeals.module.css'
+import Card from '../UI/Card';
+import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
+
 const DUMMY_MEALS = [
     {
         id: 'm1',
@@ -28,14 +31,23 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((item, index) => <li>{item.name}</li>)
+    const mealsList = DUMMY_MEALS.map((item, index) => 
+    <MealItem
+     key={item.id} 
+     meal={item}
+    //  name={item.name}
+    //  description={item.description}
+    // price={item.price}
+     />)
 
 
     return (
         <section className={classes.meals}>
-            <ul>
-                {mealsList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     )
 }
