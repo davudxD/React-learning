bdcf1979630ca23cc49d1437c97d0b08e250ebaf
davudxD <carovacdavud6@gmail.com>
2023-05-31 10:48:30 +0200

    food order app - header

---
+++ food-order-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Header from './components/Layout/Header';
+
+function App() {
+  return (
+<Fragment>
+  <Header/>
+</Fragment>
+  );
+}
+
+export default App;
