4757c83898bafd803a623fe0a865f860e60422be
davudxD <carovacdavud6@gmail.com>
2023-06-01 10:49:27 +0200

    food order app - adding Modal

--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import classes from './Cart.module.css'
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
 
 const Cart = (props) => {
     const cartItems = <ul className={classes['cart-items']}>{[{ id: 'c1', name: 'Sushi', amount: '2', price: 12.99 }].map((item, i) => {
         <li>{item.name}</li>
     })}</ul>
 
-    return <div>
+    return <Modal>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
@@ -16,7 +17,7 @@ const Cart = (props) => {
             <button className={classes['button--alt']}>Close</button>
              <button className={classes.button}>Order</button>
         </div>
-    </div>
+    </Modal>
 }
 
 export default Cart
