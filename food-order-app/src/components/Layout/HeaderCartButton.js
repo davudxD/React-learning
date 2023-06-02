de47472730eca1a2d27840e827f99075cc980aa3
davudxD <carovacdavud6@gmail.com>
2023-06-02 18:09:36 +0200

    food order app - using the context

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css'
 
 
 const HeaderCartButton = (props) => {
+const cardCtx = useContext(CartContext);
+
+const numberOfCartItems = cardCtx.items.reduce((curNumber, item) =>{
+    return curNumber + item.amount
+}, 0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>4</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     )
 }
