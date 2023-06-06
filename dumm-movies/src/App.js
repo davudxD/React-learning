2f7fd8339fdf601738af20553a0ed2afa11de484
davudxD <carovacdavud6@gmail.com>
2023-06-06 11:11:26 +0200

    Handling http errors

--- dumm-movies/src/App.js
+++ dumm-movies/src/App.js
@@ -5,12 +5,22 @@ import './App.css';
 
 function App() {
   const[movies, setMovies] = useState([]);
-  const[isLoading, setIsLoading] = useState(false)
+  const[isLoading, setIsLoading] = useState(false);
+  const[error, setError] = useState(null);
 
   async function fetchMoviesHandler(){
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films')
-    const data = await response.json()
+    setError(null);
+    try{
+      const response = await fetch('https://swapi.dev/api/films')
+     if(!response.ok){
+      throw new Error('Something went wrong!')
+    } 
+
+      // Ako response nije ok nece se izvrsit dalji kod u ovom bloku
+    // vec prelazi u catch.
+
+    const data = await response.json();
     const transformedMovies = data.results.map((item) => {
         return {
           id: item.episode_id,
@@ -20,11 +30,26 @@ function App() {
         }
       })
       setMovies(transformedMovies);
-      setIsLoading(false)
+    } catch(error){
+     setError(error.message)
+    }
+    setIsLoading(false);
   }
 
+    let content = <p>Found no movies.</p> 
 
-// useEffect(() => {fetchMoviesHandler()},[])
+    if(movies.length > 0 ) {
+      content = <MoviesList movies={movies} />
+    }
+
+    if(error) {
+      content = <p>{error}</p>
+    }
+
+    if(isLoading) {
+      content = <p style={{ fontWeight: 'bold' }}>Loading...</p>
+    }
+
 
   return (
     <React.Fragment>
@@ -32,9 +57,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0  && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
+        {content}
       </section>
     </React.Fragment>
   );
