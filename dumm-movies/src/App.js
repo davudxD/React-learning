560af995d0b0efd8804bf203eb2f345d9d61ed80
davudxD <carovacdavud6@gmail.com>
2023-06-06 19:38:04 +0200

    sending post request

--- dumm-movies/src/App.js
+++ dumm-movies/src/App.js
@@ -21,16 +21,20 @@ function App() {
     // vec prelazi u catch.
 
       const data = await response.json();
+      console.log(data);
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for(const key in data) {
+         loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+         })
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -41,8 +45,20 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+ try {
+   const res = await fetch('https://react-http-dummy-movies-63286-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
+     method: 'POST',
+     body: JSON.stringify(movie),
+     headers: {
+       'Content-Type': 'application/json'
+     }
+   });
+   const data = await res.json();
+   console.log(data)
+ } catch(error){
+  console.log(error)
+ }
   }
 
   let content = <p>Found no movies.</p>;
