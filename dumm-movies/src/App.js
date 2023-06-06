b8d5a3e9f12e659b83cb6bd8837316b483cd1a8d
davudxD <carovacdavud6@gmail.com>
2023-06-06 19:18:00 +0200

    addmovie

--- dumm-movies/src/App.js
+++ dumm-movies/src/App.js
@@ -1,70 +1,77 @@
-import React, {useState,useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
-  const[movies, setMovies] = useState([]);
-  const[isLoading, setIsLoading] = useState(false);
-  const[error, setError] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = useCallback( async() =>{
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-    try{
-      const response = await fetch('https://swapi.dev/api/films')
-     if(!response.ok){
-      throw new Error('Something went wrong!')
-    } 
-
-      // Ako response nije ok nece se izvrsit dalji kod u ovom bloku
+    try {
+      const response = await fetch('https://react-http-dummy-movies-63286-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      } 
+       // Ako response nije ok nece se izvrsit dalji kod u ovom bloku
     // vec prelazi u catch.
 
-    const data = await response.json();
-    const transformedMovies = data.results.map((item) => {
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
         return {
-          id: item.episode_id,
-          title: item.title,
-          openingText: item.opening_crawl,
-          releaseDate: item.release_date
-        }
-      })
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
       setMovies(transformedMovies);
-    } catch(error){
-     setError(error.message)
+    } catch (error) {
+      setError(error.message);
     }
     setIsLoading(false);
-  }, []) 
-  
+  }, []);
+
   useEffect(() => {
-    fetchMoviesHandler()
-  },[fetchMoviesHandler])
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
-    let content = <p>Found no movies.</p> 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
-    if(movies.length > 0 ) {
-      content = <MoviesList movies={movies} />
-    }
+  let content = <p>Found no movies.</p>;
 
-    if(error) {
-      content = <p>{error}</p>
-    }
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
 
-    if(isLoading) {
-      content = <p style={{ fontWeight: 'bold' }}>Loading...</p>
-    }
+  if (error) {
+    content = <p>{error}</p>;
+  }
 
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {content}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
 
 export default App;
+
+
