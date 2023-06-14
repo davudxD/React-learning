c3b70d4fa1a718a1f3ddc86300f338ad842e73db
davudxD <carovacdavud6@gmail.com>
2023-06-14 11:16:53 +0200

    food order app - submitting and sending cart data

--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -25,6 +25,17 @@ const Cart = (props) => {
         setIsCheckout(true);
     };
 
+    const submitOrderHandler = (userData) => {
+        fetch("https://react-http-dummy-movies-63286-default-rtdb.europe-west1.firebasedatabase.app/orders.json", {
+            method: "POST",
+            body: JSON.stringify({
+                user: userData,
+                orderedItems: cartCtx.items
+            })
+        })
+
+    }
+
     const cartItems = (
         <ul className={classes['cart-items']}>
             {cartCtx.items.map((item) => (
@@ -60,7 +71,7 @@ const Cart = (props) => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            {isCheckout && <Checkout onCancel={props.onClose} />}
+            {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />}
             {!isCheckout && modalActions}
         </Modal>
     );
