bf23803356e606f3c846289aa3be34d4cf23fe93
davudxD <carovacdavud6@gmail.com>
2023-06-17 23:16:43 +0200

    fonts

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
-
+import './App.css'
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
